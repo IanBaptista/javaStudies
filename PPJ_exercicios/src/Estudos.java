a3445fdff5273f9b5ad01c7388ff6a2f1dd4a5e7
Ian Baptista <iancbaptista@gmail.com>
2025-06-28 14:59:06 -0300

    Delete PPJ_exercicios directory

--- PPJ_exercicios/src/Estudos.java
+++
@@ -1,7 +0,0 @@
-public class Estudos {
-
-    public static void main(String[] args) {
-        System.out.println("this is the beginning of my studies, so dont bother over it");
-        System.out.println("also, best thing they taught me, was writting sout/psvm lol, eased my life");
-    }
-}
--- PPJ_exercicios/src/Perfil.java
+++
@@ -1,7 +0,0 @@
-public class Perfil{
-
-    public static void main(String[] args) {
-        System.out.println("Olá, Ian!");
-        System.out.println("Tudo bem?");
-    }
-}
